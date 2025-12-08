fb4d837d477f32c3d67654d64c2f1f6006cf44fc
ibrahim23dev <ibrahim.soft.dev23@gmail.com>
2025-12-08 16:30:11 +0600

    upload

--- problems1.js
+++ problems1.js
@@ -6,17 +6,92 @@
                 //    **
                 //     *
                
-function partanPyramid(n) {
-    for (let i = 0; i < n; i++) {
+// function partanPyramid(n) {
+//     for (let i = 0; i < n; i++) {
+//         let str = '';
+//         for (let j = 0; j < i; j++) {
+//             str += ' ';
+//         }
+//         for (let k = 0; k < n - i; k++) {
+//             str += '*';
+//         }
+//         console.log(str);
+//     }
+// }
+
+// partanPyramid(6);
+
+
+// function invertedPyramid(n) {
+//     for (let i = 0; i < n; i++) {
+//         let str = '';
+//         for (let j = 0; j < n - i - 1; j++) {
+//             str += ' ';
+//         }
+//         for (let k = 0; k <= i; k++) {
+//             str += '*';
+//         }
+//         console.log(str);
+//     }
+// }
+
+// invertedPyramid(6);
+
+
+// function diamondPattern(n) {
+//     // Upper part
+//     for (let i = 0; i < n; i++) {
+//         let str = '';
+//         for (let j = 0; j < n - i - 1; j++) {
+//             str += ' ';
+//         }
+//         for (let k = 0; k < 2 * i + 1; k++) {
+//             str += '*';
+//         }
+//         console.log(str);
+//     }
+//     // Lower part
+//     for (let i = n - 2; i >= 0; i--) {
+//         let str = '';
+//         for (let j = 0; j < n - i - 1; j++) {
+//             str += ' ';
+//         }
+//         for (let k = 0; k < 2 * i + 1; k++) {
+//             str += '*';
+//         }
+//         console.log(str);
+//     }
+// }
+// diamondPattern(5);
+
+
+// function hollowSquare(n) {
+//     for (let i = 0; i < n; i++) {
+//         let str = '';
+//         for (let j = 0; j < n; j++) {
+//             if (i === 0 || i === n - 1 || j === 0 || j === n - 1) {
+//                 str += '*';
+//             } else {
+//                 str += ' ';
+//             }
+//         }
+//         console.log(str);
+//     }
+// }
+
+// hollowSquare(6);
+
+
+function rightAngledTriangle(n) {
+    for (let i = 1; i <= n; i++) {
         let str = '';
         for (let j = 0; j < i; j++) {
-            str += ' ';
-        }
-        for (let k = 0; k < n - i; k++) {
             str += '*';
         }
         console.log(str);
     }
 }
 
-partanPyramid(6);
+rightAngledTriangle(6);
+
+
